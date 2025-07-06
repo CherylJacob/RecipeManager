03037c2096220581ad13ff3f5813cc81e0780412
Cheryl Mariam Jacob <cherylmjacob@gmail.com>
2025-07-06 17:33:23 +0200

    update

--- src/data/recipe.ts
+++ src/data/recipe.ts
@@ -1,5 +1,7 @@
 import pancakesImg from '../assets/pancake.jpg';
 import pasta from '../assets/pasta.png';
+import salad from '../assets/caprese-salad.png';
+import chocochip from '../assets/chocochip.png';
 
 interface Recipe {
     id: number;
@@ -40,30 +42,47 @@ const recipes: Recipe[] = [
     },
     {
         id: 3,
-        title: "Spaghetti Aglio e Olio",
-        description: "Simple Italian pasta with garlic and chili.",
-        image: pasta,
-        ingredients: ["Spaghetti", "Garlic", "Olive oil", "Chili flakes", "Parsley"],
-        steps: [
-            "Cook spaghetti until al dente.",
-            "Sauté garlic and chili in olive oil.",
-            "Mix spaghetti with the garlic oil, top with parsley."
+        title: "Caprese Salad",
+        image: salad,
+        description: "Healthy Salad Recipe",
+
+        ingredients: [
+            "Fresh mozzarella",
+            "Tomatoes",
+            "Fresh basil leaves",
+            "Olive oil",
+            "Balsamic glaze",
+            "Salt and pepper"
         ],
-        category: "Lunch"
+        steps: [
+            "Slice tomatoes and mozzarella",
+            "Arrange on a plate with basil leaves",
+            "Drizzle with olive oil and balsamic glaze",
+            "Season with salt and pepper to taste."
+        ], category: "Lunch"
     },
     {
         id: 4,
-        title: "Spaghetti Aglio e Olio",
-        description: "Simple Italian pasta with garlic and chili.",
-        image: pasta,
-        ingredients: ["Spaghetti", "Garlic", "Olive oil", "Chili flakes", "Parsley"],
+        title: "Chocolate Chip Pancakes",
+        description: "Yummy Chocolate Chip Pancakes",
+        image: chocochip,
+        ingredients: [
+            "1 cup flour",
+            "2 tbsp sugar",
+            "1 tsp baking powder",
+            "1/2 tsp baking soda",
+            "1 cup buttermilk",
+            "1 egg",
+            "1/2 cup chocolate chips",
+            "Butter for cooking"
+        ],
         steps: [
-            "Cook spaghetti until al dente.",
-            "Sauté garlic and chili in olive oil.",
-            "Mix spaghetti with the garlic oil, top with parsley."
+            "Mix dry ingredients",
+            " Whisk in buttermilk and egg until just combined",
+            " Fold in chocolate chips. Cook on a greased skillet until bubbles form",
+            " flip and cook until golden."
         ],
         category: "Lunch"
     }
 ];
-
 export default recipes;
